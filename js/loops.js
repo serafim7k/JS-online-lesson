a5b86fc1634f9d62946cfacf6fe403f80b318bca
serafim7k <mycomputer10j12@gmail.com>
2023-04-26 20:30:23 +0300

    loops

---
+++ js/loops.js
@@ -0,0 +1,17 @@
+let i = 0;
+
+while (i <= 6){
+    console.log(i);
+    i++;
+}
+
+let value = 0;
+
+do{
+    console.log(value);
+    value++;
+}while(value <= 6);
+
+for(let a = 0; a<3; a++){
+    console.log(a);
+}
